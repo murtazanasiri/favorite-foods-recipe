0fe4e549e539796d0744734f06fd45fbc1d3936a
murtazanasiri <mortazanasiri1991@gmail.com>
2023-11-11 16:32:27 +0200

    Implemented the CreateRecipe component for creating recipes

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,3 +1,111 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
 export const CreateRecipe = () => {
-  return <div>CreateRecipe</div>;
+  const userID = useGetUserID();
+  const [cookies, _] = useCookies(["access_token"]);
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: userID,
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(recipe);
+
+    // Convert the ingredients array to a string
+    const ingredientsString = recipe.ingredients.join(", ");
+    try {
+      await axios.post("http://localhost:3001/recipes", {
+        ...recipe,
+        ingredients: ingredientsString,
+      });
+
+      alert("Recipe Created");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
+  const handleIngredientChange = (event, index) => {
+    const { value } = event.target;
+    const ingredients = [...recipe.ingredients];
+    ingredients[index] = value;
+    setRecipe({ ...recipe, ingredients });
+  };
+
+  const handleAddIngredient = () => {
+    const ingredients = [...recipe.ingredients, ""];
+    setRecipe({ ...recipe, ingredients });
+  };
+
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={recipe.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="ingredients">Ingredients</label>
+        {recipe.ingredients.map((ingredient, index) => (
+          <input
+            key={index}
+            type="text"
+            name="ingredients"
+            value={ingredient}
+            onChange={(event) => handleIngredientChange(event, index)}
+          />
+        ))}
+        <button type="button" onClick={handleAddIngredient}>
+          Add Ingredient
+        </button>
+
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          value={recipe.instructions}
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          value={recipe.imageUrl}
+          onChange={handleChange}
+        />
+        <label htmlFor="cookingTime">Cooking Time</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          value={recipe.cookingTime}
+          onChange={handleChange}
+        />
+        <button type="submit">Create Recipe</button>
+      </form>
+    </div>
+  );
 };
