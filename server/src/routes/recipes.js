982ad8de2681b513fa8a76b86166d70bde883a47
murtazanasiri <mortazanasiri1991@gmail.com>
2023-11-11 11:43:05 +0200

    Implemented the backed for recipe and added three more endpoints for recipe

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipesModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -18,6 +19,39 @@ router.post("/", async (req, res) => {
   try {
     const response = await recipe.save();
     res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipesModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.get("/savedRecipes/ids", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    res.json({ savedRecipes: user?.savedRecipes });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.get("/savedRecipes", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    const savedRecipes = await RecipesModel.find({
+      _id: { $in: user.savedRecipes },
+    });
+    res.json({ savedRecipes });
   } catch (error) {
     res.json(error);
   }
